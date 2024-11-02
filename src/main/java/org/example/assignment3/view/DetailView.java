2e241ea0cd364597fcc0829d9ee8ef6654864eea
Pratham Jigar Shah (mvr659) <mvr659@mail.usask.ca>
2024-11-02 06:19:05 -0600

    Made a starter code for DetailView.java file with help of lab provided code fragments for now. This is going to be just for testing purpose on how things work with each other and react to different purpose.

--- src/main/java/org/example/assignment3/view/DetailView.java
+++ src/main/java/org/example/assignment3/view/DetailView.java
@@ -1,4 +1,48 @@
 package org.example.assignment3.view;
 
-public class DetailView {
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import org.example.assignment3.InteractionModel;
+import org.example.assignment3.controller.AppController;
+import org.example.assignment3.model.EntityModel;
+import org.example.assignment3.model.Subscriber;
+
+public class DetailView extends StackPane implements Subscriber {
+    double hieght, width;
+    GraphicsContext gc;
+    EntityModel model;
+    InteractionModel imodel;
+
+    public DetailView() {
+        hieght = 600;
+        width = 800;
+        Canvas myCan = new Canvas(width, hieght);
+        gc = myCan.getGraphicsContext2D();
+        getChildren().add(myCan);
+    }
+
+    public void setModel(EntityModel mdl) {
+        model = mdl;
+    }
+    public void setIModel(InteractionModel imdl) {
+        imodel = imdl;
+    }
+    public void draw() {
+        gc.clearRect(0, 0, width, hieght);
+        model.getEntities().forEach(ntt -> {
+            gc.setFill(Color.BLUE);
+            gc.fillOval(ntt.getX() - ntt.getR(), ntt.getY() - ntt.getR(), ntt.getR() *2, ntt.getR() *2);
+        });
+    }
+    public void modelChanged() {
+        draw();
+    }
+
+    public void setupEvents(AppController ctrlr) {
+        setOnMousePressed(ctrlr::handlePressed);
+        setOnMouseReleased(ctrlr::handleReleased);
+        setOnMouseDragged(ctrlr::handledDragged);
+    }
 }

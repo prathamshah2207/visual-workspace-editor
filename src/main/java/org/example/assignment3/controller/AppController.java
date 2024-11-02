84245f75bb352c5138701c86f5a77c9c88055686
Pratham Jigar Shah (mvr659) <mvr659@mail.usask.ca>
2024-11-02 07:56:40 -0600

    made a basic functional controller that handles the states as per the click events and just creates circles right now. Also selection using the Interaction model and finally dragging it accross the canvas works mildly.

--- src/main/java/org/example/assignment3/controller/AppController.java
+++ src/main/java/org/example/assignment3/controller/AppController.java
@@ -7,7 +7,8 @@ import org.example.assignment3.model.InteractionModel;
 public class AppController {
 
     private enum State {READY, MOVING, PREPARE_CREATE}
-    private State currentState = State.READY;
+    private State currentState;
+    private double x, y, dX, dY;
     private EntityModel model;
     private InteractionModel imodel;
 
@@ -25,6 +26,16 @@ public class AppController {
     public void handlePressed(MouseEvent event) {
         switch (currentState) {
             case READY:
+                x = event.getX();
+                y = event.getY();
+
+                if (model.contains(event.getX(), event.getY())) {
+                    imodel.setSelected(model.whichObject(event.getX(), event.getY()));
+                    currentState = State.MOVING;
+                }
+                else {
+                    currentState = State.PREPARE_CREATE;
+                }
         }
         System.out.println("Mouse Pressed");
     }
@@ -32,7 +43,9 @@ public class AppController {
     public void handleReleased(MouseEvent event) {
         switch (currentState) {
             case PREPARE_CREATE:
+                currentState = State.READY;
             case MOVING:
+                currentState = State.READY;
         }
         System.out.println("Mouse Released");
     }
@@ -40,7 +53,13 @@ public class AppController {
     public void handleDrag(MouseEvent event) {
         switch (currentState) {
             case PREPARE_CREATE:
+                model.addObject(event.getX(), event.getY());
             case MOVING:
+                dX = event.getX() - x;
+                dY = event.getY() - y;
+                x = event.getX();
+                y = event.getY();
+                model.moveObject(imodel.getSelected(), dX, dY);
         }
         System.out.println("Mouse Drag");
     }
